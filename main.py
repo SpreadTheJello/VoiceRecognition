f08ba22aba4570e19291ccc12181cae8a6190240
SpreadTheJello <28422680+SpreadTheJello@users.noreply.github.com>
2021-09-16 10:58:07 -0700

    BaseException Errors

--- main.py
+++ main.py
@@ -7,23 +7,24 @@ USERPATH = "--user-data-dir=/Users/mrjello/Desktop/CSE-Projects/VoiceAssistance/
 
 op = webdriver.ChromeOptions()
 op.add_argument(USERPATH)
+#op.add_argument('--headless')
 op.add_experimental_option("excludeSwitches", ["enable-automation"])
 op.add_experimental_option('useAutomationExtension', False)
 op.page_load_strategy = 'normal'
-driver = webdriver.Chrome(PATH, options=op)
 
 recognizer = sr.Recognizer()
 
 while True:
     try:
         with sr.Microphone() as mic:
-            recognizer.adjust_for_ambient_noise(mic, duration=0.5)
+            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
             audio = recognizer.listen(mic)
-
             text = recognizer.recognize_google(audio)
             text = text.lower()
+            print(text)
 
             if(text == "open youtube"):
+                driver = webdriver.Chrome(PATH, options=op)
                 driver.get('https://www.youtube.com/')
                 time.sleep(5)
 
