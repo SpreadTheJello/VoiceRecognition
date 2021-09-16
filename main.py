984642423996f708e285e841cf89fec7ce97f0d5
SpreadTheJello <28422680+SpreadTheJello@users.noreply.github.com>
2021-09-16 10:33:07 -0700

    Project Started

---
+++ main.py
@@ -0,0 +1,32 @@
+import speech_recognition as sr
+from selenium import webdriver
+import time
+
+PATH = "/Users/mrjello/Downloads/chromedriver"
+USERPATH = "--user-data-dir=/Users/mrjello/Desktop/CSE-Projects/VoiceAssistance/UserData"
+
+op = webdriver.ChromeOptions()
+op.add_argument(USERPATH)
+op.add_experimental_option("excludeSwitches", ["enable-automation"])
+op.add_experimental_option('useAutomationExtension', False)
+op.page_load_strategy = 'normal'
+driver = webdriver.Chrome(PATH, options=op)
+
+recognizer = sr.Recognizer()
+
+while True:
+    try:
+        with sr.Microphone() as mic:
+            recognizer.adjust_for_ambient_noise(mic, duration=0.5)
+            audio = recognizer.listen(mic)
+
+            text = recognizer.recognize_google(audio)
+            text = text.lower()
+
+            if(text == "open youtube"):
+                driver.get('https://www.youtube.com/')
+                time.sleep(5)
+
+    except sr.UnknownValueError():
+        recognizer = sr.Recognizer()
+        continue
