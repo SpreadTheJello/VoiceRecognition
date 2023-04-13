c2649644dfb6dd54c0ddef113d70aa9ced22ae50
Angelo Fatali <afatali@ucmerced.edu>
2023-04-12 20:02:47 -0700

    added comments

--- commands.py
+++ commands.py
@@ -6,11 +6,12 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-
+# Function to click an element after a small delay
 def click(element):
     time.sleep(0.1)
     element.click()
 
+# Function to get a random video URL from the current page
 def get_random_video_url(browser):
     def _get_random_video_url():
         url = "https://www.youtube.com"
@@ -19,25 +20,28 @@ def get_random_video_url(browser):
         page_source = browser.page_source
         soup = BeautifulSoup(page_source, "html.parser")
 
+        # Find all video links on the page
         video_links = [a["href"] for a in soup.find_all("a", href=True) if a["href"].startswith("/watch?v=")]
 
-
+        # If no video links found, return None
         if not video_links:
             return None
         
+        # Get the first 8 video links (prevents unseen videos being clicked) and choose a random one
         video_links = video_links[:8]
-        
         random_video_url = url + random.choice(video_links)
         print(random_video_url)
         return random_video_url
     return _get_random_video_url
 
+# Function to open YouTube homepage
 def open_youtube(browser):
     def _open_youtube():
         browser.get("https://www.youtube.com/")
         print("--opened youtube--")
     return _open_youtube
 
+# Function to play or pause the current video
 def play_pause(browser):
     def _play_pause():
         body = browser.find_element(By.TAG_NAME, 'body')
@@ -45,6 +49,7 @@ def play_pause(browser):
         print("--paused/played video--")
     return _play_pause
 
+# Function to play a random video from the homepage
 def homepage_random_video(browser):
     def _homepage_random_video():
         random_video_url = get_random_video_url(browser)()
@@ -55,6 +60,7 @@ def homepage_random_video(browser):
             print("Failed to find a random video")
     return _homepage_random_video
 
+# Function to play the next video in the queue
 def next_video(browser):
     def _next_video():
         body = browser.find_element(By.TAG_NAME, 'body')
@@ -62,6 +68,7 @@ def next_video(browser):
         print("--played next video--")
     return _next_video
 
+# Function to mute or unmute the current video
 def mute_video(browser):
     def _mute_video():
         body = browser.find_element(By.TAG_NAME, 'body')
@@ -69,6 +76,7 @@ def mute_video(browser):
         print("--muted/unmuted video--")
     return _mute_video
 
+# Function to navigate to the YouTube homepage
 def homepage(browser):
     def _homepage():
         open_youtube_func = open_youtube(browser)
@@ -76,18 +84,21 @@ def homepage(browser):
         print("--went to homepage--")
     return _homepage
 
+# Function to navigate back in the browser history
 def back_button(browser):
     def _back_button():
         browser.execute_script("window.history.go(-1)")
         print("--back button pressed--")
     return _back_button
 
+# Function to navigate forward in the browser history
 def forward_button(browser):
     def _forward_button():
         browser.execute_script("window.history.go(1)")
         print("--forward button pressed--")
     return _forward_button
 
+# Main function to run the script
 def main():
     driver_path = "/path/to/chromedriver"
     browser = webdriver.Chrome(driver_path)
--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import commands as cmd
 from neuralintents import GenericAssistant
 from selenium import webdriver
 
+# Path to the ChromeDriver executable
 driver_path = "/path/to/chromedriver"
 
-
+# Create a browser instance using the ChromeDriver
 browser = webdriver.Chrome(driver_path)
 
+# Define the command-to-function mappings
 mappings = {
     "open_youtube": cmd.open_youtube(browser),
     "play_pause": cmd.play_pause(browser),
@@ -19,30 +21,32 @@ mappings = {
     "mute_video": cmd.mute_video(browser)
 }
 
+# Initialize the assistant with the command intents and the mappings
 assistant = GenericAssistant('command_intents.json', intent_methods=mappings)
+
+# Uncomment these when commands are added/removed
 # assistant.train_model()
 # assistant.save_model()
+
 assistant.load_model()
 
-# use microphone 1 as the audio source
-
-# print("Available microphones:")
-# for index, name in enumerate(sr.Microphone.list_microphone_names()):
-#     print(f"Index: {index}, Name: {name}")
-
-# channel = int(input("Enter the index of the desired microphone: "))
-
+# Create a recognizer instance
 r = sr.Recognizer()
 
+# Continuous voice command processing loop
 while True:
     with sr.Microphone(device_index=1) as mic:
+        # Adjust for ambient noise
         r.adjust_for_ambient_noise(mic)
         print("\nListening...")
+        # Record audio for 4 seconds
         audio = r.listen(mic, phrase_time_limit=4)
 
     try:
+        # Recognize speech using Google Speech Recognition
         input = r.recognize_google(audio)
         input = input.lower()
+        # Get the response from the assistant
         response = assistant.request(input)
         if response != "I don't know how to handle that command":
             print("You said: " + input)
